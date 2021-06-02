87f1279e2a7d97e2956c30be13cd2cd790418c25
Gargi58 <81302812+Gargi58@users.noreply.github.com>
2021-06-02 12:35:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -51,19 +51,19 @@ function setup() {
   
 	 
  	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
- 	boxleftSprite.shapeColor=color(255,0,0);
+ 	boxleftSprite.shapeColor="darkgreen";
 
  	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
  	World.add(world, boxLeftBody);
 
  	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
- 	boxBase.shapeColor=color(255,0,0);
+ 	boxBase.shapeColor="darkgreen";
 
  	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
  	World.add(world, boxBottomBody);
 
  	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
- 	boxleftSprite.shapeColor=color(255,0,0);
+ 	boxleftSprite.shapeColor="darkgreen";
 
  	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
  	World.add(world, boxRightBody);
